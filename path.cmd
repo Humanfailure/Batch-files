50e7ff783e0bd5d3dba8c80601845c4f9fb86b2c
KekoaM <kai.moritamcvey@gmail.com>
2018-10-09 20:42:07 -0700

    Commented code

--- path.cmd
+++ path.cmd
@@ -1,3 +1,15 @@
 @echo off
+rem this tells the command line to not display commands such as echo
+
+echo current path:
 path
-echo a
+rem this shows the current path string
+
+set /p id="Enter ID: "
+rem this sets the variable id to what the user inputs, the /p command denotes the variable id as a string of text
+
+pause
+rem pauses untill the user inputs
+
+echo ypur id is: %id%
+rem this shows the variable id in the terminal as text
