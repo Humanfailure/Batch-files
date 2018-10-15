b88028e54590008a368626a8791260479e9dc34f
Kai <KekoaM@users.noreply.github.com>
2018-10-15 15:02:30 -0700

    Fixed many things, broke some other things

--- path.cmd
+++ path.cmd
@@ -1,24 +1,25 @@
 @echo off
-rem this tells the command line to not display commands such as echo
-rem the text "rem" designates a line as a comment and does not show it
+::this tells the command line to not display commands such as echo
+:: the text "::" designates a line as a comment and does not show it
 
 echo current path:
 path
-rem this shows the current path string
+:: this shows the current path string
 
 set /p id="Enter folder to add to path: "
-rem this sets the variable id to what the user inputs, the /p command denotes the variable id as a string of text
+:: this sets the variable id to what the user inputs, the /p command denotes the variable id as a string of text
 
 set syspath=""
-rem sets the "syspath" variable to only local path (this is done by not giving a value for syspath)
+:: sets the "syspath" variable to only local path (this is done by not giving a value for syspath)
  
 set /p syspath="Type /M for system wide path, hit ENTER to only set local path "
-rem asks user to determine if system wide path will also be changed
-
-rem path = %path%; %id%
-rem setx 
-
-echo %syspath%
+:: asks user to determine if system wide path will also be changed
+set PATH=%PATH%;%id%  
 
 echo "%id%"  added to path
-rem this shows the variable id in the terminal as text
+:: this shows the variable id in the terminal as text
+
+path
+
+set path=%path:C:\Users\Kai Morita-McVey\AppData\Local\Programs\Python\Python37-32%
+path
